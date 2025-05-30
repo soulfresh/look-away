99616daff62908cea3e5b6fc83e8cf7a6b84fad9
robert m wren <things@soul-fresh.com>
2025-05-30 16:50:14 -0400

    feat: simple menu bar app
    Shows an eye icon in the system menu bar with a quit option.

--- LookAway/LookAwayApp.swift
+++ LookAway/LookAwayApp.swift
@@ -9,9 +9,30 @@ import SwiftUI
 
 @main
 struct LookAwayApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        Settings {
+            EmptyView()
         }
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var statusItem: NSStatusItem?
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+        if let button = statusItem?.button {
+            button.image = NSImage(systemSymbolName: "eye", accessibilityDescription: "Look Away")
+        }
+        // Create the menu
+        let menu = NSMenu()
+        menu.addItem(NSMenuItem(title: "Quit LookAway", action: #selector(quitApp), keyEquivalent: "q"))
+        menu.items.last?.target = self
+        statusItem?.menu = menu
+    }
+
+    @objc func quitApp() {
+        NSApplication.shared.terminate(nil)
     }
 }
